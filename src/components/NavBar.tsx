def9b6bedf979395654014b971737d671448df2b
unknown <142760953+scentier@users.noreply.github.com>
2023-09-11 14:46:52 +0700

    ganti tailwind, bootswatch gak jalan

---
+++ src/components/NavBar.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function NavBar() {
+  return (
+    <>
+      <nav
+        className="text-3xl font-bold underline text-red-900"
+        data-bs-theme="dark"
+      >
+        NAVBAR
+      </nav>
+    </>
+  );
+}
