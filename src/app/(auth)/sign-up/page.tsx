89ba71dc0ec591340a5eafafeb9e31532ca1b5e3
unknown <142760953+scentier@users.noreply.github.com>
2023-09-17 17:33:51 +0700

    next api add new book

--- src/app/(auth)/sign-up/page.tsx
+++ src/app/(auth)/sign-up/page.tsx
@@ -3,7 +3,7 @@ import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
 import { TSignUpSchema, signUpSchema } from "@/lib/types";
 
-export default function NewBook() {
+export default function SignUp() {
   const {
     register,
     handleSubmit,
--- src/app/api/book/route.ts
+++ src/app/api/book/route.ts
@@ -11,6 +11,7 @@ export async function POST(request: Request) {
   const result = newBookSchema.safeParse(body);
 
   let zodErrors = {};
+  let statusMsg = {};
   if (!result.success) {
     result.error.issues.forEach((issue) => {
       zodErrors = { ...zodErrors, [issue.path[0]]: issue.message };
@@ -20,35 +21,26 @@ export async function POST(request: Request) {
     const bookObj: TBook = result.data;
     const slug = slugify(bookObj.title);
     bookObj.slug = slug;
-    console.log("route.ts>bookObj====", bookObj);
+    try {
+      const isExist = await Book.findOne({ slug: slug });
+      if (!isExist) {
+        await Book.create(bookObj);
+        statusMsg = { ...statusMsg, message: "New Book Added!" };
+      } else {
+        statusMsg = { ...statusMsg, message: "Book already exist!" };
+      }
+    } catch (error) {
+      const err = error as Record<string, unknown>;
+      statusMsg = { ...statusMsg, message: err.message };
+    }
   }
+  console.log("statusMsg route.ts:", statusMsg);
 
   return NextResponse.json(
     Object.keys(zodErrors).length > 0
       ? { errors: zodErrors }
-      : { success: true }
+      : { success: true, message: statusMsg }
   );
-}
-
-export async function post_mongo(req: NextRequest, res: NextResponse) {
-  const bookObj: TBook = await req.json();
-  const slug = slugify(bookObj.title);
-  bookObj.slug = slug;
-  try {
-    const isExist = await Book.findOne({ slug: slug });
-    if (!isExist) {
-      await Book.create(bookObj);
-      return NextResponse.json(
-        { message: "Add success!", data: bookObj },
-        { status: 201 }
-      );
-    } else {
-      return NextResponse.json({ message: "exist!", data: bookObj });
-    }
-  } catch (error) {
-    const e = error as Record<string, unknown>;
-    return NextResponse.json({ message: e.message, data: bookObj });
-  }
 }
 
 export async function GET() {
