0609053fc5b7e379da2a385d040e3ea5616b3d45
unknown <142760953+scentier@users.noreply.github.com>
2023-09-18 09:40:12 +0700

    update

--- src/app/api/book/[bookId]/route.ts
+++ src/app/api/book/[bookId]/route.ts
@@ -8,26 +8,38 @@ type Props = {
   params: { bookId: string };
 };
 
-export async function PUT(req: NextRequest, { params: { bookId } }: Props) {
-  const {
-    ttl: title,
-    slg: slug,
-    des: description,
-    aut: author,
-    dwl: download,
-    pub: published,
-  } = await req.json();
-
-  await Book.findByIdAndUpdate(bookId, {
-    title,
-    slug,
-    description,
-    author,
-    download,
-    published,
-  });
-  return NextResponse.json({ message: "update ok!" }, { status: 200 });
+export async function PUT(request: Request, { params: { bookId } }: Props) {
+  try {
+    const body = await request.json();
+    console.log("body=======================", body);
+    await Book.findByIdAndUpdate(bookId, body);
+    return NextResponse.json({ message: "update ok!" }, { status: 200 });
+  } catch (error) {
+    console.log(error);
+    return NextResponse.json({ message: "catch error" }, { status: 400 });
+  }
 }
+
+// export async function PUT(req: NextRequest, { params: { bookId } }: Props) {
+//   const {
+//     ttl: title,
+//     slg: slug,
+//     des: description,
+//     aut: author,
+//     dwl: download,
+//     pub: published,
+//   } = await req.json();
+
+//   await Book.findByIdAndUpdate(bookId, {
+//     title,
+//     slug,
+//     description,
+//     author,
+//     download,
+//     published,
+//   });
+//   return NextResponse.json({ message: "update ok!" }, { status: 200 });
+// }
 
 export async function GET(req: NextRequest, { params: { bookId } }: Props) {
   const getBook = await Book.findOne({ _id: bookId });
--- src/components/BookDetail.tsx
+++ src/components/BookDetail.tsx
@@ -8,14 +8,14 @@ export default function BookDetail({ book }: Props) {
       <h1>{book.title}</h1>
       <h3>{book.author}</h3>
       <div className="content">{book.description}</div>
-      {book.tags.length > 0 && (
+      {typeof book.tags != "undefined" && book.tags.length > 0 ? (
         <p>
           tags:{" "}
           {book.tags.map((tag) => (
             <span key={tag}>{tag}, </span>
           ))}
         </p>
-      )}
+      ) : null}
       <p>First published: {book.published}</p>
     </article>
   );
